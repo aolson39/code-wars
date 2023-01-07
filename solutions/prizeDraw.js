0086e061a5c6145aca2405aa49a18af26447bbe5
Adam Olson <adamolson0123@gmail.com>
2023-01-06 17:39:24 -0800

    prizeDraw

---
+++ solutions/prizeDraw.js
@@ -0,0 +1,104 @@
+// To participate in a prize draw each one gives his/her firstname.
+
+// Each letter of a firstname has a value which is its rank in the English alphabet. A and a have rank 1, B 
+//and b rank 2 and so on.
+
+// The length of the firstname is added to the sum of these ranks hence a number som.
+
+// An array of random weights is linked to the firstnames and each som is multiplied by its corresponding 
+//weight to get what they call a winning number.
+
+// Example:
+// names: "COLIN,AMANDBA,AMANDAB,CAROL,PauL,JOSEPH"
+// weights: [1, 4, 4, 5, 2, 1]
+
+// PauL -> som = length of firstname + 16 + 1 + 21 + 12 = 4 + 50 -> 54
+// The *weight* associated with PauL is 2 so PauL's *winning number* is 54 * 2 = 108.
+// Now one can sort the firstnames in decreasing order of the winning numbers. When two people have the same 
+//winning number sort them alphabetically by their firstnames.
+
+// Task:
+// parameters: st a string of firstnames, we an array of weights, n a rank
+
+// return: the firstname of the participant whose rank is n (ranks are numbered from 1)
+
+// Example:
+// names: "COLIN,AMANDBA,AMANDAB,CAROL,PauL,JOSEPH"
+// weights: [1, 4, 4, 5, 2, 1]
+// n: 4
+
+// The function should return: "PauL"
+// Notes:
+// The weight array is at least as long as the number of names, it may be longer.
+
+// If st is empty return "No participants".
+
+// If n is greater than the number of participants then return "Not enough participants".
+
+// See Examples Test Cases for more examples.
+
+
+
+// names: "COLIN,AMANDBA,AMANDAB,CAROL,PauL,JOSEPH"
+// weights: [1, 4, 4, 5, 2, 1]
+
+// PauL -> som = length of firstname + 16 + 1 + 21 + 12 = 4 + 50 -> 54
+// The *weight* associated with PauL is 2 so PauL's *winning number* is 54 * 2 = 108.
+
+function rank(st, we, n) {
+    // your code
+    let ranks = []
+    st = st.split(',')
+    for(let i = 0; i < st.length; i++){
+        let wordRank = wordValue(st[i]) * we[i]
+        console.log(wordRank);
+        ranks.push([st[i],wordRank])
+    }
+    ranks.sort(function(a, b) {
+        return a[1] - b[1];
+    }); 
+    return ranks[n-1]
+}
+
+function wordValue(name){
+    var letterMap = {
+        a: 1,
+        b: 2,
+        c: 3,
+        d: 4,
+        e: 5,
+        f: 6,
+        g: 7,
+        h: 8,
+        i: 9,
+        j: 10,
+        k: 11,
+        l: 12,
+        m: 13,
+        n: 14,
+        o: 15,
+        p: 16,
+        q: 17,
+        r: 18,
+        s: 19,
+        t: 20,
+        u: 21,
+        v: 22,
+        w: 23,
+        x: 24,
+        y: 25,
+        z: 26
+    }
+    let result = name.length;
+    let arr = name.split('')
+    //lowercase all letters in array
+    arr = arr.map(x =>  x.toLowerCase())
+    console.log(arr);
+    //map letter to value and add all values
+    return arr.reduce((acc, letter) => acc + letterMap[letter],result)
+
+}
+
+console.log(rank("COLIN,AMANDBA,AMANDAB,CAROL,PauL,JOSEPH", [1, 4, 4, 5, 2, 1], 3));
+console.log(rank("Addison,Jayden,Sofia,Michael,Andrew,Lily,Benjamin", [4, 2, 1, 4, 3, 1, 2], 4));
+console.log(rank("Lagon,Lily", [1, 5], 2));
